a754c4b1f52f82e35d403154100e2a653b5e6eb4
Victor <victorsmirnov@me.com>
2020-05-09 22:03:26 +0300

    xxx

--- twitter_nlp.py
+++ twitter_nlp.py
@@ -2,5 +2,24 @@ import tweepy
 from textblob import TextBlob
 
 
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+public_tweets = api.search('Tim Cook')
+for tweet in public_tweets:
+    print(tweet.text)
+
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
+    print('\n')
+
+
 if __name__ == "__main__":
     pass
