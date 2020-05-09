51f89fac3413d6826bb4953d9204558c84791671
Victor <victorsmirnov@me.com>
2020-05-09 22:09:36 +0300

    install

--- ttt.py
+++ ttt.py
@@ -1,3 +1,10 @@
 from textblob import TextBlob
 
 
+text = 'Для начала, мы выведем самые последние твиты, связанные с нашим ключевым словом. После мы используем textblob, чтобы определить тональность каждого отдельного твита и выведем её тоже. Посмотрим, что сказал глава Apple Тим Кук.'
+
+print(text)
+
+analysis = TextBlob(text)
+print(analysis.sentiment)
+print('\n')
